978948d0cf9bf36e298f3f6f87243cf2e86bd776
unknown <soumitriroy05@gmail.com>
2026-07-20 11:47:33 +0530

    Add Dynamic Programming solutions

---
+++ DYNAMIC_PROGRAMMING/src/CLIMBING_STAIRS.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CLIMBING_STAIRS
+{
+    static int climbing(int n)
+    {
+        if(n<=2)
+        {
+            return n;
+        }
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(climbing(n));
+    }
+}
